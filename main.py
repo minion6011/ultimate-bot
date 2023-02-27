12b32029cbfb5d1c4ed265d2f13d98489be3cfa4
minion6011 <74075005+minion6011@users.noreply.github.com>
2023-02-27 21:11:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -724,6 +724,20 @@ async def get_message_id(ctx, message: discord.Message):  # message commands ret
 
 #modal start
 
+
+class MyModal(discord.ui.Modal):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.add_item(discord.ui.InputText(label="Short Input"))
+        self.add_item(discord.ui.InputText(label="Long Input", style=discord.InputTextStyle.long))
+
+    async def callback(self, interaction: discord.Interaction):
+        embed = discord.Embed(title="Modal Results")
+        embed.add_field(name="Short Input", value=self.children[0].value)
+        embed.add_field(name="Long Input", value=self.children[1].value)
+        await interaction.response.send_message(embeds=[embed])
+
 @client.slash_command(name="suggestion")
 async def suggestion(ctx: discord.ApplicationContext):
 	modal = MyModal(title="test")
