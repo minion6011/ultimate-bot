165ec01f24ebe5b608e32a7082cdaf2f8dbfd467
minion6011 <74075005+minion6011@users.noreply.github.com>
2023-03-04 12:57:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -757,8 +757,8 @@ async def suggestion(ctx: discord.ApplicationContext):
 #apllication command discord.py
 
 @client.tree.context_menu(name="Get Message ID") #message contex command
-async def getmessageid(ctx, message: discord.Message):
-    await ctx.response(f"***Message ID: ***`{message.id}`", ephemeral=True)
+async def getmessageid(interaction: discord.Interaction, message: discord.Message):
+    await interaction.response.send_message(f"***Message ID: ***`{message.id}`", ephemeral=True)
 
 @client.tree.command(name = "suggestion", description = "Suggest a command for Ultimate-Bot") #slash command
 async def suggestion(interaction: discord.Interaction):
