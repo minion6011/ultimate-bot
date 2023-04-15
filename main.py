44c8ff254608db2bab541fe787644cd3ad890532
minion6011 <74075005+minion6011@users.noreply.github.com>
2023-04-15 09:24:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -927,10 +927,9 @@ async def suggestion(interaction: discord.Interaction):
 
 from discord import app_commands
 
+
+
 @client.tree.command(name="giveaway", description = "Make a giveaway (immediately)") #slash command
-@app_commands.describe(
-    prize='The prize that you wanna give in givweaway',
-)
 async def giweaway(interaction: discord.Interaction, prize: str):
 		embed = discord.Embed(title=":tada: Giveaway :tada:", color=0xe91e63)
 		results = [member for member in interaction.guild.members if not member.bot]
