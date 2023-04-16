4ec723360aab6a2f277659afaa7781351d83894b
minion6011 <74075005+minion6011@users.noreply.github.com>
2023-04-16 22:10:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -880,7 +880,7 @@ async def giveaway3(ctx):
     else:
         users = []
         async for user in reactions[0].users():
-            if user != bot.user:
+            if user == user.bot:
                 users.append(user)
         if not users:
             await ctx.send('No one entered the giveaway, so there is no winner!')
