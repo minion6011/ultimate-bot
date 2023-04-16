354e948dcb4ce2a721a9add7b348951df9fd08b3
minion6011 <74075005+minion6011@users.noreply.github.com>
2023-04-16 11:09:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -872,8 +872,8 @@ async def giveaway2(ctx, seconds: int, *, prize: str):
 	if time > 300:
 		await ctx.send("error max num")
 	else:
-		await ctx.send(f"React with :tada: to enter the giveaway for **{prize}**! Time remaining: **{time}** seconds.")
-		message = ctx.message
+		message = await ctx.send(f"React with :tada: to enter the giveaway for **{prize}**! Time remaining: **{time}** seconds.")
+		#message = ctx.message
 		await message.add_reaction("<:checkmark_2714fe0f:1073342463995023433>")
 		await asyncio.sleep(time)
 		await ctx.fetch_message(message.id)
