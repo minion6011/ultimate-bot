e46a4931d22573280d1bf779c9c5f156b3f92391
minion6011 <74075005+minion6011@users.noreply.github.com>
2023-04-25 08:00:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1072,6 +1072,128 @@ async def timeout(ctx, member: discord.Member, until: int):
 #youtube
 import youtube_dl
 
+    @client.command(name='play', aliases=['p'], pass_context = True)
+    async def play(ctx, *, search_term:str = None):
+        if ctx.author.voice:
+            voice = None
+            if search_term == None:
+                await ctx.send('No song specified.')
+                return
+            if not ctx.voice_client:
+                channel = ctx.message.author.voice.channel
+                voice = await channel.connect()
+            else:
+                voice = ctx.guild.voice_client
+            
+            url = YT.singleSearch(search_term)
+            
+            YTDLP_OPTIONS = {
+                'format': 'bestaudio/best',
+                'extractaudio': True,
+                'audioformat': 'mp3',
+                'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+                'restrictfilenames': True,
+                'noplaylist': True,
+                'nocheckcertificate': True,
+                'ignoreerrors': False,
+                'logtostderr': False,
+                'quiet': True,
+                'no_warnings': True,
+                'default_search': 'ytsearch',
+                'source_address': '0.0.0.0',
+            }
+'''
+ =====>     FFMPEG_OPTIONS = {
+                'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+                'options': '-vn',
+            }
+'''
+            with yt_dlp.YoutubeDL(YTDLP_OPTIONS) as ydl:
+                info = ydl.extract_info(url, download=False)
+                playUrl = info['url']
+
+            source = FFmpegPCMAudio(source=playUrl,before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",options="-vn")
+            voice.play(source)
+        else:
+            await ctx.send('You must be in a voice channel to play a song!')
+            return
+
+
+    @client.command(name='play', aliases=['p'], pass_context = True)
+    async def play(ctx, *, search_term:str = None):
+        if ctx.author.voice:
+            voice = None
+            if search_term == None:
+                await ctx.send('No song specified.')
+                return
+            if not ctx.voice_client:
+                channel = ctx.message.author.voice.channel
+                voice = await channel.connect()
+            else:
+                voice = ctx.guild.voice_client
+            
+            url = YT.singleSearch(search_term)
+            
+            YTDLP_OPTIONS = {
+                'format': 'bestaudio/best',
+                'extractaudio': True,
+                'audioformat': 'mp3',
+                'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+                'restrictfilenames': True,
+                'noplaylist': True,
+                'nocheckcertificate': True,
+                'ignoreerrors': False,
+                'logtostderr': False,
+                'quiet': True,
+                'no_warnings': True,
+                'default_search': 'ytsearch',
+                'source_address': '0.0.0.0',
+            }
+
+ =====>     FFMPEG_OPTIONS = {
+                'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+                'options': '-vn',
+            }
+
+            with yt_dlp.YoutubeDL(YTDLP_OPTIONS) as ydl:
+                info = ydl.extract_info(url, download=False)
+                playUrl = info['url']
+
+            source = FFmpegPCMAudio(playUrl, options=FFMPEG_OPTIONS)
+            voice.play(source)
+        else:
+            await ctx.send('You must be in a voice channel to play a song!')
+            return
+
+    @client.command(pass_context = True)
+    async def leave(ctx):
+        if ctx.voice_client:
+            await ctx.guild.voice_client.disconnect()
+        else:
+            await ctx.send("I'm not in a voice channel!")
+
+    @client.command(pass_context = True)
+    async def pause(ctx):
+        voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
+        if voice.is_playing():
+            voice.pause()
+        else:
+            await ctx.send('No audio playing...')
+
+    @client.command(pass_context = True)
+    async def resume(ctx):
+        voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
+        if voice.is_paused():
+            voice.resume()
+        else:
+            await ctx.send('No audio paused...')
+
+    @client.command(pass_context = True)
+    async def stop(ctx):
+        voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
+        voice.stop()
+
+'''
 ytdlopts = { 
     'format': 'bestaudio/best',
     'outtmpl': 'downloads/%(extractor)s-%(id)s-%(title)s.%(ext)s',
@@ -1131,6 +1253,8 @@ async def play(ctx, *, query):
         print(e)
 
     await ctx.send(f'**Now playing:** {title}') #sending the title of the video
+'''
+
 #youtube
 
 @tasks.loop(seconds=18)
