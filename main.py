d770d18fdb05b147756cfd27e253d7fe595a05a7
minion6011 <74075005+minion6011@users.noreply.github.com>
2023-04-25 16:18:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1072,52 +1072,79 @@ async def timeout(ctx, member: discord.Member, until: int):
 #youtube
 import youtube_dl
 from discord import FFmpegPCMAudio
-import responses
-import youtubeSearch as YT
-import yt_dlp
 
-@client.command()
-async def play(ctx, *, search_term:str = None):
-	if ctx.author.voice:
-		voice = None
-		if search_term == None:
-			await ctx.send('No song specified.')
-			return
-		if not ctx.voice_client:
-			channel = ctx.message.author.voice.channel
-			voice = await channel.connect()
-		else:
-			voice = ctx.guild.voice_client
+ytdl_format_options = {
+  
+  "format": "bestaudio/best",
 
-		url = YT.singleSearch(search_term)
+  "outtmpl": "%(extractor)s-%(id)s-%(title)s.%(ext)s",
 
-		YTDLP_OPTIONS = {
-			'format': 'bestaudio/best',
-			'extractaudio': True,
-			'audioformat': 'mp3',
-			'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-			'restrictfilenames': True,
-			'noplaylist': True,
-			'nocheckcertificate': True,
-			'ignoreerrors': False,
-			'logtostderr': False,
-			'quiet': True,
-			'no_warnings': True,
-			'default_search': 'ytsearch',
-			'source_address': '0.0.0.0',
-		}
-		
-		with yt_dlp.YoutubeDL(YTDLP_OPTIONS) as ydl:
-			info = ydl.extract_info(url, download=False)
-			playUrl = info['url']
+  "restrictfilename": True,
 
-		source = FFmpegPCMAudio(source=playUrl,before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",options="-vn")
-		voice.play(source)
-		
-	else:
-            await ctx.send('You must be in a voice channel to play a song!')
-            return
+  "noplaylist": True,
 
+  "nocheckcertificate": True,
+
+  "ignoreerrors": False,
+
+  "logtostderr": False,
+
+  "quiet": True,
+
+  "no_warning": True,
+
+  "default_search": "auto",
+
+  "source_address": "0.0.0.0",
+
+}
+
+ffmpeg_options = {"options": "-vn"}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
+#this function will get a download from a Url to allow the bot get youtube Url and download that and play it through the bot
+class YTDLSource(discord.PCMVolumeTransformer):
+  def__init__(self, source: *, data, volume = 0.5):
+    super().__init__(source, volume)
+
+    self.data = data
+    self.title = data.get("title")
+    self.url = data.get("url")
+
+#this method is basically returning the file so it can be played through the bot
+  @classmethod
+  async def from_url(cls, url, *, loop=None, stream=False):
+    loop = loop or asyncio.get_event_loop()
+    data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+    if "entries" in data:
+      data = data["entries"][0]
+
+    filename = data["url"] if stream else ytdl.prepare_filename(data)
+    return cls(nextcord.FFmpegAudio(filename, **ffmpeg_options), data=data)
+
+#this class holds all the commands for the music bot
+class Music(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+
+
+#this method joins the command and makes the connection to the channel(on your bot enter $join to join channel) 
+  @commands.command()
+  async def join(self, ctx, *, channel: discord.VoiceChannel):
+    if ctx.voice_client is not None:
+      return await ctx.voice_client.move_to(channel)
+    await channel.connect()
+
+#method to automate the bot to play the file it fetches
+  @commands.command()
+  async def play(self, ctx, *, query):
+    source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
+    ctx.voice_client.play(source, after= lambda e: print(f"player error": {e}") if e else None)
+
+    await ctx.send(f"Now playing: {query}")
 '''
     @client.command(name='play', aliases=['p'], pass_context = True)
     async def play(ctx, *, search_term:str = None):
