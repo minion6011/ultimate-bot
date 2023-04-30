2b50b8c916227d5b25cf1c03b028555a38d4b525
minion6011 <74075005+minion6011@users.noreply.github.com>
2023-04-30 14:48:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -973,7 +973,7 @@ chatgpt= Conversation()
 
 
 @client.tree.command(name="gpt", description = "chat gpt the message") #message contex command
-async def chat_gpt(interaction: discord.Interaction, question: str):
+async def chat_gpt(interaction: discord.Interaction, question):
 	response = chatgpt.chat(question)
 	await interaction.response.send_message(f"{response}")
     
