bcb45a97579eb44a6825d4e4ff3d65c34d1efa85
minion6011 <74075005+minion6011@users.noreply.github.com>
2023-05-01 11:28:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1047,7 +1047,7 @@ async def servers(ctx):
 	for guild in client.guilds:
 		channel = guild.text_channels[0]
 		invite = await channel.create_invite()
-		message += f"> ***`{guild.name}` (id: `{guild.id}`) membri: `{guild.member_count}`\nLink invito: [url]({invite.url}) ***\n\n"
+		message += f" > ***`{guild.name}` (id: `{guild.id}`) membri: `{guild.member_count}`\n Link invito: {invite.url} ***\n\n"
 	await ctx.send(message)
 	
 	
