a5695b1f5fc86fbb8136de225a18661a035e257a
minion6011 <74075005+minion6011@users.noreply.github.com>
2023-05-10 19:50:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1118,45 +1118,6 @@ async def play2(ctx, url):
 	try:
 		# Download the video
 		video = pytube.YouTube(url)
-		#video.streams.filter(file_extension='3gpp').first().dowload()
-		video.streams.first().download()
-		
-		# Get the voice channel of the user who typed the command
-		voice = await ctx.author.voice.channel.connect()
-		
-		# Join the voice channel
-		
-		# Play the video
-		#filename = f"{video.title}"
-		#files = glob.glob(f"{filename}.*")
-		#if files:
-		#file_extension = files[0].split(".")[-1]
-		#file_name = video.title + '.' + file.mime_type.split('/')[-1]
-		source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(f"{video.title}.3gpp"))
-		voice.play(source)
-		await ctx.send("video start")
-		
-		# Wait for the video to finish playing
-		while voice.is_playing():
-			await asyncio.sleep(1)
-			
-		# Disconnect from the voice channel
-		await voice.disconnect()
-		
-		# Delete the video file
-		os.remove(f"{video.title}.3gpp")
-	except Exception as e:
-		await voice.disconnect()
-		print(e)
-		channel = client.get_channel(errorchannel)
-		await channel.send(f"**[Errore]** \naudio isinstance: ```{e}```")
-		raise error
-		
-@client.command()
-async def play3(ctx, url):
-	try:
-		# Download the video
-		video = pytube.YouTube(url)
 		video.streams.first().download()
 		
 		# Get the voice channel of the user who typed the command
@@ -1182,6 +1143,41 @@ async def play3(ctx, url):
 		
 		# Delete the video file
 		os.remove(f"{video.default_filename}")
+	except Exception as e:
+		print(e)
+		await ctx.send("An error occurred while playing the video.")
+		
+		
+@client.command()
+async def play3(ctx, url):
+	try:
+		# Download the video
+		video = pytube.YouTube(url)
+		video.streams.first().download()
+		
+		# Get the voice channel of the user who typed the command
+		voice_channel = ctx.author.voice.channel
+		
+		# Join the voice channel
+		voice = await voice_channel.connect()
+		
+		# Play the video
+		#filename = f"{video.title}"
+		#files = glob.glob(f"{filename}.*")
+		#if files:
+		#file_extension = files[0].split(".")[-1]
+		source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(video.title + ".3gpp"))
+		voice.play(source)
+		
+		# Wait for the video to finish playing
+		while voice.is_playing():
+			await asyncio.sleep(1)
+			
+		# Disconnect from the voice channel
+		await voice.disconnect()
+		
+		# Delete the video file
+		os.remove(video.title + ".3gpp")
 	except Exception as e:
 		print(e)
 		await ctx.send("An error occurred while playing the video.")
