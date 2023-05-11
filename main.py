e820e5f9c207ac7d45e751da4531c9d41166ab45
minion6011 <74075005+minion6011@users.noreply.github.com>
2023-05-11 22:54:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1112,7 +1112,6 @@ import pytube
 import asyncio
 import os
 
-playback_states = {}
 
 @client.command()
 async def play(ctx, url):
@@ -1136,35 +1135,6 @@ async def play(ctx, url):
 			# Get the voice channel of the user who typed the command
 			voice_client = ctx.guild.voice_client
 			if voice_client and voice_client.is_connected():
-				voice_channel = ctx.author.voice.channel
-				voice = ctx.voice_client
-				# Join the voice channel
-				#voice = await voice_channel.connect()
-
-				#info
-				embed = discord.Embed(title=f"***Title: ***```{video.title}```", color=discord.Colour.red())
-				embed.set_image(url=video.thumbnail_url)
-				embed.set_footer(text=footer_testo)
-				await msg.delete()
-				#await msg.edit(embed=embed)
-				await ctx.send(embed=embed)
-
-
-				# Play the video
-				source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(video.title + ".3gpp"))
-				voice.play(source)
-
-				# Wait for the video to finish playing
-				while voice.is_playing():
-					await asyncio.sleep(1)
-
-				# Disconnect from the voice channel
-				await voice.disconnect()
-
-				# Delete the video file
-				os.remove(video.title + ".3gpp")
-
-			else:
 				voice_channel = ctx.author.voice.channel
 
 				# Join the voice channel
