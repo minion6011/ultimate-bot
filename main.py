551a460779307e0894c0fe6c4d2554ccd17792d5
minion6011 <74075005+minion6011@users.noreply.github.com>
2023-05-20 09:23:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1137,14 +1137,12 @@ async def play(ctx, url):
 	
 	voice_state = ctx.author.voice
 	if voice_state is None:
-		await asyncio.sleep(0.5)
-		no_voice_embed = discord.Embed(title=f"*** You are not currently in a voice channel. ***", color=discord.Colour.red())
-		no_voice_embed.set_footer(text=footer_testo)
-		await ctx.send(embed=no_voice_embed)
-		await asyncio.sleep(0.5)
+		embed = discord.Embed(title="*** You are not currently in a voice channel. ***", color=discord.Colour.red())
+		embed.set_footer(text=footer_testo)
+		await ctx.send(embed=embed)
 	else:
 		if ctx.voice_client is not None and ctx.voice_client.is_playing():
-			no_music_embed = discord.Embed(title=f"*** Please wait until the song is finished to start another one, If you want to stop the song you can use ```?stop``` ***", color=discord.Colour.red())
+			no_music_embed = discord.Embed(title="*** Please wait until the song is finished to start another one, If you want to stop the song you can use ```?stop``` ***", color=discord.Colour.red())
 			no_music_embed.set_footer(text=footer_testo)
 			await ctx.send(embed=no_music_embed)
 		else:
