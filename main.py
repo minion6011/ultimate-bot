6c20e6cac8deb6f558c53d6fb99f6e654faa7818
minion6011 <74075005+minion6011@users.noreply.github.com>
2023-05-27 10:08:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,10 @@ beta_list = [598119406731657216, 829022689338851389]
 
 
 is_me = commands.check(lambda ctx: ctx.author.id == my_id) 
-is_beta = str(ctx.author.id) in [str(id) for id in beta_list]
+
+is_beta = str(message.author.id) in [str(id) for id in beta_list]
+if not is_beta:
+    await message.author.send("Non sei ancora autorizzato ad accedere alla funzionalità beta.")
 
 
 intents = discord.Intents.default()
