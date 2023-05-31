f043a2a819e6aa4458363768946e16da23448875
minion6011 <74075005+minion6011@users.noreply.github.com>
2023-05-31 15:40:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1195,7 +1195,6 @@ async def automod3(ctx):
     
     await ctx.guild.create_automod_rule(name="Profanity Filter By Me lol", trigger=auto_mod_trigger, actions=[a_single_object_for_this], event_type=auto_mod_event)
 	
-	
 '''	
 @is_me
 @client.command()
