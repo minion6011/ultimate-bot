29eeae125b7aca4d5403843d28cf1ba8dba41352
minion6011 <74075005+minion6011@users.noreply.github.com>
2023-05-31 16:33:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1183,18 +1183,33 @@ async def custom_emoji_info(ctx, emoji: discord.Emoji = None):
 
 @is_beta
 @client.command()
+@client.command()
 async def automod3(ctx):
-    auto_mod_trigger = discord.AutoModTrigger(
-        type=discord.AutoModRuleTriggerType.keyword_preset,
-        presets=discord.AutoModPresets(profanity=True)
+    # Crea un trigger personalizzato per la parola "spam"
+    trigger = discord.AutoModTrigger(
+        type=discord.AutoModRuleTriggerType.keyword,
+        keyword_filter=["spam"]
     )
-    
-    a_single_object_for_this = discord.AutoModRuleAction(discord.AutoModRuleActionType.block_message)
-    
-    auto_mod_event = discord.AutoModRuleEventType.message_send
-    
-    await ctx.guild.create_automod_rule(name="Profanity Filter By Me lol", trigger=auto_mod_trigger, actions=[a_single_object_for_this], event_type=auto_mod_event)
 
+    # Crea un'azione personalizzata per bloccare i messaggi contenenti la parola "spam"
+    action = discord.AutoModRuleAction(
+        action_type=discord.AutoModRuleActionType.block_message,
+        custom_message="Messaggi contenenti la parola 'spam' non sono ammessi in questo canale."
+    )
+
+    # Crea una regola di AutoMod personalizzata utilizzando il trigger e l'azione creati
+    rule = discord.AutoModRule(
+        name="spam_filter",
+        event_type=discord.AutoModRuleEventType.message_send,
+        trigger=trigger,
+        actions=[action]
+    )
+
+    # Aggiungi la regola di AutoMod personalizzata al canale corrente
+    await ctx.channel.create_automod_rule(rule=rule)
+
+    # Invia un messaggio di conferma al canale
+    await ctx.send("Regola di AutoMod creata con successo!")
 '''	
 @is_me
 @client.command()
