57820e1985d42dcaacbba079483500a38cf75e8d
minion6011 <74075005+minion6011@users.noreply.github.com>
2023-06-20 22:36:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -231,13 +231,13 @@ async def on_voice_state_update(member, before, after):
 		await asyncio.sleep(20)
 		channel = client.get_channel(stalkid)
 		embed = discord.Embed(title=f"**[Stalker]**\nUtente in Chat vocale\nUtente: `{member.display_name}#{member.discriminator}`\nServer: `{member.guild.name}`", color=discord.Color.blue())
-		embed.add_field(name = 'Canale:', value=f"<#{after.channel.id}>", inline = True)
+		embed.add_field(name = 'Canale:', value=f"<#{after.channel}>", inline = True)
 		await channel.send(embed=embed)
 	if before.channel is not None and after.channel is None:
 		await asyncio.sleep(20)
 		channel = client.get_channel(stalkid)
 		embed = discord.Embed(title=f"**[Stalker]**\nUtente in Chat vocale\nUtente: `{member.display_name}#{member.discriminator}`\nServer: `{member.guild.name}`", color=discord.Color.red())
-		embed.add_field(name = 'Canale:', value="Nessuno", inline = True)
+		embed.add_field(name = 'Canale:', value=f"<#{before.channel}>", inline = True)
 		await channel.send(embed=embed)
 	
 
