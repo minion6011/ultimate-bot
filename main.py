e156ab943553b9586ab868392e3cf7f925cf70fe
Minion6011 <mvittorio29@gmail.com>
2023-08-10 13:27:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -364,7 +364,7 @@ async def unlock(ctx):
 @commands.guild_only()
 @commands.has_permissions(manage_messages=True)
 async def nuke(ctx, amount: int = 100):
-	if amount > 500:
+	if amount < 500:
         	embed = discord.Embed(title=f"{amount} messages deleted", color=discord.Color.red())
         	embed.set_image(url="https://www.19fortyfive.com/wp-content/uploads/2021/10/Nuclear-Weapons-Test.jpg")
         	await ctx.channel.purge(limit=amount + 1)
